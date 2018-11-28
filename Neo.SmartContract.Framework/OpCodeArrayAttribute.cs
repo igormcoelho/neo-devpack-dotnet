fb6727b12aac26e7c54c3b38a13ea5271035eeb1
Igor Machado <igor.machado@gmail.com>
2018-11-27 23:12:13 -0200

    opcode array test

---
+++ Neo.SmartContract.Framework/OpCodeArrayAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Neo.VM;
+using System;
+
+namespace Neo.SmartContract.Framework
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Constructor)]
+    public class OpCodeArrayAttribute : Attribute
+    {
+        public OpCode[] vOpCode { get; }
+
+        public OpCodeArrayAttribute(OpCode[] vopcode)
+        {
+            this.vOpCode = vopcode;
+        }
+    }
+}
