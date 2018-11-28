81b4dcd98b67be5867d2f2ce5385696c2c860a06
Igor Machado <igor.machado@gmail.com>
2018-11-28 16:25:35 -0200

    more var

--- Neo.SmartContract.Framework/OpCodeAttribute.cs
+++ Neo.SmartContract.Framework/OpCodeAttribute.cs
@@ -7,10 +7,12 @@ namespace Neo.SmartContract.Framework
     public class OpCodeAttribute : Attribute
     {
         public OpCode OpCodeX { get; }
+        public byte OpCodeY { get; }
 
         public OpCodeAttribute(OpCode opcode)
         {
             this.OpCodeX = opcode;
+            this.OpCodeY = (byte)10;
         }
     }
 }
