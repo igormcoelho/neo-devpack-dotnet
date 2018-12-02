dbe6b939aef354910131ac33d5d6bb21d9539de9
Igor Machado <igor.machado@gmail.com>
2018-12-02 12:51:16 -0200

    inline attributes

---
+++ Neo.SmartContract.Framework/InlineAttribute.cs
@@ -0,0 +1,48 @@
+﻿using Neo.VM;
+using System;
+
+namespace Neo.SmartContract.Framework
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Constructor)]
+    public class InlineAttribute : Attribute
+    {
+        public OpExt[] Operations { get; }
+
+        public InlineAttribute(params OpExt[] ops)
+        {
+            this.Operations = ops;
+        }
+    }
+
+    // OpCode with Extension parameters
+    public class OpExt
+    {
+        public OpCode Code; // unused
+        public byte[] Parameters; // unused
+        public string SParameters; // unused
+
+        public OpExt(OpCode Code, byte[] Parameters = null)
+        {
+            this.Code = Code;
+            this.Parameters = Parameters;
+            this.SParameters = null;
+        }
+
+        public OpExt(string call)
+        {
+            this.Code = OpCode.SYSCALL;
+            this.Parameters = null;
+            this.SParameters = call;
+        }
+
+        public static implicit operator OpExt(OpCode code)
+        {
+            return new OpExt(code);
+        }
+
+        public static implicit operator OpExt(string call)
+        {
+            return new OpExt(call);
+        }
+    }
+}
